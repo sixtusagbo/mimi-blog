fec9ab689f47943140624f556216444927cd12dc
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-18 00:16:34 +0100

    Handle fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [posts, setPosts] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8001/posts")
-      .then(res => res.json())
+      .then(res => {
+        if (!res.ok) {
+          throw Error("Could not fetch posts");
+        }
+        return res.json();
+      })
       .then(data => {
         setPosts(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
         setIsLoading(false);
       })
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {posts && <BlogList posts={posts} title="All Posts" />}
     </div>
