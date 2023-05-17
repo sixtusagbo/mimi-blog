df0b277425d695d8533ea8b6becd67ec32b2de07
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-17 20:58:38 +0100

    Respond to click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Clicked", e);
+  }
+
+  const handleClickAgain = (noun, e) => {
+    console.log(`Hello ${noun}!`, e.target);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button><br />
+      <button onClick={(e) => handleClickAgain("World", e)}>Click me again o</button>
     </div>
   );
 }
