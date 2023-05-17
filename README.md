03ecbaa5a74a373f5dbe9e79176200c770912b96
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-18 00:37:09 +0100

    Refactor with custom hook

--- README.md
+++ README.md
@@ -2,14 +2,24 @@
 
 Simple blog project with Create, Read, Update and Delete (CRUD) features written in React.
 
+## Getting Started
+Launch the app with:
+```bash
+npm run serve & npm run start
+```
+
 ## Available Scripts
 
 In the project directory, you can run:
 
-### `npm start`
+### `npm run start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### `npm run serve`
+
+Starts the file storage json-server.\
+Open [http://localhost:80001/posts](http://localhost:80001/posts) to view it in your browser.
 
 ### `npm run build`
 
--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,8 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [posts, setPosts] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8001/posts")
-      .then(res => {
-        if (!res.ok) {
-          throw Error("Could not fetch posts");
-        }
-        return res.json();
-      })
-      .then(data => {
-        setPosts(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch(err => {
-        setError(err.message);
-        setIsLoading(false);
-      })
-  }, []);
+  const { data: posts, isLoading, error } = useFetch("http://localhost:8001/posts");
 
   return (
     <div className="home">
