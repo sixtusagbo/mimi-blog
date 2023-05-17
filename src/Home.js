136c192d1c84962314ccb922b1c4ef3f2caa41b8
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-17 23:46:27 +0100

    Show conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,20 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [posts, setPosts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8001/posts")
       .then(res => res.json())
       .then(data => {
         setPosts(data);
+        setIsLoading(false);
       })
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {posts && <BlogList posts={posts} title="All Posts" />}
     </div>
   );
