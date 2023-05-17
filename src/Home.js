f5b1e3c14c24e0b0dc2227ad6b214d2ca39b2773
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-17 21:41:37 +0100

    Display list of posts

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-  let [name, setName] = useState("Miralo");
-  let [age, setAge] = useState(19);
-
-  const handleNameChange = () => {
-    setName("Sixtus");
-  };
-
-  const incrementAge = () => setAge(age += 1);
+  const [posts, setPosts] = useState([
+    { title: "This is post one", body: "lorem ipsum ...", author: "Sixtus", id: 1 },
+    { title: "This is post two", body: "lorem ipsum ...", author: "Miralo", id: 2 },
+    { title: "This is post three", body: "lorem ipsum ...", author: "Miracle", id: 3 },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>My name is {name}.</p>
-      <p>I am {age} years old.</p>
-      <button onClick={handleNameChange}>Change my name</button>
-      <button onClick={incrementAge}>Increase age</button>
+      {posts.map((post) => (
+        <div className="post_preview" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>Written by {post.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
