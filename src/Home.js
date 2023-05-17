fa31bb5195ca5aa7b771d5f7e32ec272963b7046
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-17 22:01:11 +0100

    Reuse components

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,14 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [posts, setPosts] = useState([
     { title: "This is post one", body: "lorem ipsum ...", author: "Sixtus", id: 1 },
-    { title: "This is post two", body: "lorem ipsum ...", author: "Miralo", id: 2 },
-    { title: "This is post three", body: "lorem ipsum ...", author: "Miracle", id: 3 },
+    { title: "This is post two", body: "lorem ipsum ...", author: "Miracle", id: 2 },
+    { title: "This is post three", body: "lorem ipsum ...", author: "Sixtus", id: 3 },
   ]);
 
   return (
     <div className="home">
       <BlogList posts={posts} title="All Posts" />
+      <BlogList posts={posts.filter((post) => post.author === "Sixtus")} title="Sixtus' posts" />
     </div>
   );
 }
