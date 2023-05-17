1d2e8d2793f54bd50f9298425b22d75321fe71ad
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-17 20:41:30 +0100

    Add css styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,13 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>Mimi Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
+        <a href="/" style={{
+          color: "#fff",
+          backgroundColor: "#f1356d",
+          borderRadius: "8px",
+          paddingLeft: "12px",
+          paddingRight: "12px",
+        }}>Home</a>
         <a href="/create">New Post</a>
       </div>
     </nav>
