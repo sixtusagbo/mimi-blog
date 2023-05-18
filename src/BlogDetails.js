a2b7e6e6c828faa0e2feebcbf512f857a0e1ecb1
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-18 23:24:43 +0100

    Display post details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: post, error, isLoading } = useFetch(`http://localhost:8001/posts/${id}`);
 
   return (
-    <div className="blog_details">
-      <h2>Post {id} Details</h2>
+    <div className="post_details">
+      {isLoading && <div>Loading post detail...</div>}
+      {error && <div>{error}</div>}
+      {post && (
+        <article>
+          <h2>{post.title}</h2>
+          <p>Written by {post.author}</p>
+          <div>{post.body}</div>
+        </article>
+      )}
     </div>
   );
 }
