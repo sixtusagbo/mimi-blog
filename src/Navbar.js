4b81cb99fe7a1d3bb337866e84134ff46e112e0c
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-18 22:41:15 +0100

    Intercept server requests to improve speed

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Mimi Blog</h1>
       <div className="links">
-        <a href="/" style={{
+        <Link to="/" style={{
           color: "#fff",
           backgroundColor: "#f1356d",
           borderRadius: "8px",
           paddingLeft: "12px",
           paddingRight: "12px",
-        }}>Home</a>
-        <a href="/create">New Post</a>
+        }}>Home</Link>
+        <Link to="/create">New Post</Link>
       </div>
     </nav>
   );
