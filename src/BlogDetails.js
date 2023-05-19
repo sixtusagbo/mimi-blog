4cc81bbed4d50c9587bae5dba65904fb0ea3d559
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-05-19 01:08:36 +0100

    Add delete post functionality

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,16 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: post, error, isLoading } = useFetch(`http://localhost:8001/posts/${id}`);
+  const history = useHistory();
+
+  const handleDelete = () => {
+    fetch(`http://localhost:8001/posts/${post.id}`, {
+      method: 'DELETE'
+    }).then(() => history.push('/'));
+  }
 
   return (
     <div className="post_details">
@@ -14,6 +21,7 @@ const BlogDetails = () => {
           <h2>{post.title}</h2>
           <p>Written by {post.author}</p>
           <div>{post.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
